54dbd065a3b7f9a744cb5f52d0f72c424ec77628
DevakiNandan8711 <devakinandan076@gmail.com>
2026-03-02 15:39:59 +0530

    fix: resolve map rendering issue by correcting EJS syntax and scoping

--- public/js/map.js
+++ public/js/map.js
@@ -1,13 +1,13 @@
-maptilersdk.config.apiKey = mapToken;
+maptilersdk.config.apiKey = window.mapToken;
 
 const map = new maptilersdk.Map({
   container: 'map', // container's id or the HTML element to render the map
   style: maptilersdk.MapStyle.STREETS,
-  center: coordinates,
+  center: window.coordinates,
   zoom: 9,
 });
 
 const marker = new maptilersdk.Marker({ color: "red" })
-  .setLngLat(coordinates)
-  .setPopup(new maptilersdk.Popup({ offset: 25 }).setHTML(`<h4>${locationName}</h4><p>Exact location of booked StayNest</p>`))
+  .setLngLat(window.coordinates)
+  .setPopup(new maptilersdk.Popup({ offset: 25 }).setHTML(`<h4>${window.locationName}</h4><p>Exact location of booked StayNest</p>`))
   .addTo(map);
